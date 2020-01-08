3656fae0ee97d1ec985a5a038bc1e53f50c925c9
Patrick Stock <41335494+flybrains@users.noreply.github.com>
2020-01-08 11:19:05 -0500

    Add files via upload

--- pc/server.py
+++ pc/server.py
@@ -1,8 +1,12 @@
 import sys
 import argparse
 import time
+import csv
+import logging
+import serial
 import numpy as np
 import config
+from datetime import datetime
 from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM
 
 
@@ -12,20 +16,26 @@ def establish_pi_connection(RPI_HOST, RPI_PORT):
 	return sock
 
 def convert_angle_for_arduino(inputVal, previousAngle, mult):
+
 	inputVal = inputVal*(256/800)
 	spr = 800
 	conv1 = spr*(1000/256)
 	newAngle1 = (inputVal*conv1)/1000
-	highLimit = 450
-	lowLimit = 350
+	
+
+	highLimit = 500
+	lowLimit = 300
 	midPoint = 400
 	offset = mult*spr
 
-	if (newAngle1 < highLimit) and (newAngle1 >lowLimit):
+	if (newAngle1 < highLimit) and (newAngle1 > lowLimit):
+
 		if(newAngle1 > midPoint):
 			newAngle1 = highLimit
 		else:
 			newAngle1 = lowLimit
+
+
 	newAngle1 = newAngle1 + offset
 
 	if np.abs(newAngle1-previousAngle) > 400:
@@ -40,12 +50,26 @@ def convert_angle_for_arduino(inputVal, previousAngle, mult):
 			newAngle1 = newAngle1 + spr
 
 	previousAngle = newAngle1
+	
 
-	return newAngle1, mult
+	return int(newAngle1), mult
 
 def run_fictrac_client(LOCAL_HOST, LOCAL_PORT, RPI_HOST, RPI_PORT):
 
+
 	RPI_SOCK  = establish_pi_connection(RPI_HOST, RPI_HOST)
+	time.sleep(1)
+
+	# create logger
+	now = datetime.now() # current date and time
+	dts = now.strftime("%m%d%Y-%H%M%S")
+
+	logger = logging.getLogger('closed_loop_client_output')
+	hdlr = logging.FileHandler(r'C:\Users\rutalab\Desktop\fictrac-data\client-logs\{}.log'.format(dts))
+	logger.addHandler(hdlr) 
+	logger.setLevel(logging.DEBUG)
+	logger.info('{} ` {}, {}, {}, {}, {}, {}, {}, {}, {}'.format("timestamp", "ft_heading", "ft_xPos","ft_yPos", "motor_step_command", "mfc1_stpt", "mfc2_stpt", "mfc3_stpt", "led1_stpt", "led2_stpt"))
+
 
 	with socket(AF_INET, SOCK_STREAM) as sock:
 		time.sleep(0.02)
@@ -54,6 +78,7 @@ def run_fictrac_client(LOCAL_HOST, LOCAL_PORT, RPI_HOST, RPI_PORT):
 		mfc1_sp, mfc2_sp, mfc3_sp = 0.0, 0.0, 0.0
 		led1_sp, led2_sp = 0.0, 0.0
 		motorSendVal = 800000
+		lastMotorSendVal = motorSendVal
 		expStartTime = time.time()
 		LEDLastTime = expStartTime
 		odorLastTime = expStartTime
@@ -66,6 +91,7 @@ def run_fictrac_client(LOCAL_HOST, LOCAL_PORT, RPI_HOST, RPI_PORT):
 		mult = 1000
 
 		try:
+			i = 0
 			while True:
 				data = sock.recv(1024)
 				if not data:
@@ -73,42 +99,59 @@ def run_fictrac_client(LOCAL_HOST, LOCAL_PORT, RPI_HOST, RPI_PORT):
 				line = data.decode('UTF-8')
 				toks = line.split(',')
 
+
 				if ((len(toks) < 24) | (toks[0] != "FT")):
-					print('Bad read')
+					('Bad read')
 					continue
+
 
 				posx = float(toks[15])*3
 				posy = -float(toks[16])*3
+				print(posy)
 				net_vel = float(toks[19])*3
 				heading = float(toks[17])
-				propHead = heading/6.28
+
+				
+				heading = -heading
+				motorHeadingMap = heading % (2*np.pi)
+				propHead = motorHeadingMap/(2*np.pi)
 				target = int(propHead*800)
 
 				if len(slidingWindow) >= config.SLIDING_WINDOW_LENGTH:
 					slidingWindow.pop(0)
 				slidingWindow.append([posx, posy, net_vel, heading])
 
-				if (time.time() - motorLastTime) > (1/30):
+				if (time.time() - motorLastTime) > (1/60000):
 
 					correctedTarget, mult = convert_angle_for_arduino(target, previousAngle, mult)
+
 					previousAngle, motorSendVal = correctedTarget, correctedTarget
 					motorLastTime = time.time()
 
 					if config.PROPORTIONAL_ODOR==False:
-						mfc1_sp = float(config.FLOW_RATE / 1000)	#air
-						mfc2_sp = 0.0								#odor1
-						mfc3_sp = 0.0								#odor2
+			
+						mfc1_sp = 0.0								#empty
+						mfc2_sp = (float(config.FLOW_RATE) / 666.0)*(float(config.PERCENT_AIR)/100.0) #air
+						mfc3_sp = (float(config.FLOW_RATE) / 666.0)*(float(config.PERCENT_ACV)/100.0) #acv
+					
 					else:
 						if config.N_ODOR_SOURCES == 1:
-							if posy >= config.SOURCE:
-								mfc1_sp = float(config.MAX_TOTAL_AIRFLOW/1000)
+							if posy < 0:
+								mfc1_sp = 0.0
+								mfc2_sp = float(config.MAX_TOTAL_AIRFLOW)/666.0
+								mfc3_sp = 0.0
+
+							elif posy >= config.SINGLE_SOURCE_DISTANCE:
+								mfc1_sp = 0.0
 								mfc2_sp = 0.0
-								mfc3_sp = 0.0
+								mfc3_sp = float(config.MAX_TOTAL_AIRFLOW)/666.0
 							else:
-								mfc1_sp = float((config.MAX_TOTAL_AIRFLOW / 1000)*(posy/config.SOURCE_1_Y_DISTANCE))
-								mfc1_sp = max(0, mfc1_sp)
-								mfc2_sp = float((config.MAX_TOTAL_AIRFLOW / 1000)*(1-(posy/config.SOURCE_1_Y_DISTANCE)))
-								mfc3_sp = 0.0
+								mfc1_sp = 0
+								mfc2_sp = (float(config.MAX_TOTAL_AIRFLOW) / 666.0)*float(1-(posy/config.SINGLE_SOURCE_DISTANCE))
+								mfc3_sp = max(0, mfc2_sp)
+								mfc3_sp = (float(config.MAX_TOTAL_AIRFLOW) / 666.0)*float(posy/config.SINGLE_SOURCE_DISTANCE)
+								mfc3_sp = max(0, mfc3_sp)
+								
 
 					if config.LED_ACTIVATION:
 						if config.STIMULATION_MODE == 't':
@@ -168,14 +211,16 @@ def run_fictrac_client(LOCAL_HOST, LOCAL_PORT, RPI_HOST, RPI_PORT):
 						led2_sp = 0.0
 
 					SENDSTRING = '<'+ '{},{},{},{},{},{},{}'.format(1,motorSendVal, mfc1_sp, mfc2_sp, mfc3_sp, led1_sp, led2_sp) +'>\n'
-					RPI_SOCK.sendto(str.encode('{}'.format(SENDSTRING)), (RPI_HOST, RPI_PORT))
-			else:
-				pass
+					RPI_SOCK.sendto(str.encode('{}'.format(SENDSTRING)), ("192.168.137.10", 5000))
+					now = datetime.now()
+					dts = now.strftime("%m/%d/%Y-%H:%M:%S.%f")
+					logger.info('{} -- {}, {}, {}, {}, {}, {}, {}, {}, {}'.format(dts,heading, posx,posy, motorSendVal, mfc1_sp, mfc2_sp, mfc3_sp, led1_sp, led2_sp))
 
+	
 		except KeyboardInterrupt:
-			SENDSTRING = '<'+ '{},{},{},{},{},{},{}'.format(0, mfcVal1, mfcVal2, mfcVal3) +'>\n'
+			SENDSTRING = '<'+ '{},{},{},{},{},{},{}'.format(0, 'a', mfc1_sp, mfc2_sp, mfc3_sp, led1_sp, led2_sp) +'>\n'
 			RPI_SOCK.sendto(str.encode('{}'.format(SENDSTRING)), (RPI_HOST, RPI_PORT))
-
+			RPI_SOCK.close()
 
 if __name__=='__main__':
 	config.check_cfg()
