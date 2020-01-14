0e8ef35a775ad7052c729e7486f019f9b9e9c1be
Patrick Stock <41335494+flybrains@users.noreply.github.com>
2020-01-14 16:53:30 -0500

    Update config.py

--- pc/config.py
+++ pc/config.py
@@ -2,47 +2,46 @@
 # IP and port for the laptop, to establish socket communication with
 # FicTrac. Get the IP by running ipconfig on this computer, and look
 # for the port in the FicTrac configuration file.
-LOCAL_HOST = '000.000.000.000'
-LOCAL_PORT = 5000
+LOCAL_HOST = '192.168.137.1'
+LOCAL_PORT = 3100
 # ==============================================================================
 # IP and port for connecting to Raspberry Pi. Set the port manually here
 # Get the IP by running ifconfig on the Raspberry Pi.
-RPI_HOST = '000.000.000.000'
-RPI_PORT = 3000
+RPI_HOST = '192.168.137.10'
+RPI_PORT = 5000
 # ==============================================================================
 # If you want to run a closed loop experiment with constant flow, set True
-# and provide a flowrate (Max = 1000 mL/min)
+# and provide a flowrate (Max = 666 mL/min)
 CONSTANT_FLOW = True
-FLOW_RATE = 500
+FLOW_RATE = 600
+PERCENT_AIR = 100
+PERCENT_ACV = 0
 # ==============================================================================
 # If you want to run a closed loop experiment with propoartional odor, set True
 # and provide the number of sources and their x/y positions.
 # If number of sources is 1, the parameters of source 2 are disregarded,
 # as well as the x position for source 1.
 PROPORTIONAL_ODOR = False
-MAX_TOTAL_AIRFLOW = 600
+MAX_TOTAL_AIRFLOW = 400
 N_ODOR_SOURCES = 1
-SOURCE_1_Y_DISTANCE = 20  #SET ONLY THIS ONE IF DOING SINGLE SOURCE EXPERIMENT
-SOURCE_1_X_DISTANCE = 20
-SOURCE_1_RADIUS = 5
-SOURCE_2_Y_DISTANCE = 20
-SOURCE_2_X_DISTANCE = 20
-SOURCE_2_RADIUS = 5
+SINGLE_SOURCE_DISTANCE= 1000 #SET ONLY THIS ONE IF DOING SINGLE SOURCE EXPERIMENT
+REPEATING_STRIPS = False
+XON = 100 # Strip width
 # ==============================================================================
 # If you want to activate LEDs at any time, set LED activation to True
 # Set LED intensity as a fraction of maximum voltage
-LED_ACTIVATION = True
+LED_ACTIVATION = False
 LED_COLOR = 'red'
-LED_INTENSITY = 0.5
+LED_INTENSITY = 3.0
 # ............................................................................
 # LED stimulation can be temporally-activated (on a fixed program) or
 # conditionally-activated (based on a behavioral characteristic)
 STIMULATION_MODE = 't'    # t for temporal, c for conditional
 # ............................................................................
 # If you choose to use LEDs and use them temporally, fill out out the following.
-INITIAL_LED_DELAY = 60
+INITIAL_LED_DELAY = 1
 PERIOD_BETWEEN_PULSE_RISING_EDGES = 30
-DURATION_OF_PULSE_T = 2
+DURATION_OF_PULSE_T = 30
 # ............................................................................
 # If you choose to use LEDs and use them conditionally, fill out out the following.
 # The following behavioral characteristics can be used to activate light
